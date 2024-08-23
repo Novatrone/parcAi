bb796aa958e40846dee63b8e3386339e7a580f31
craftandcode <craftandcode01@gmail.com>
2024-08-23 17:43:09 +0530

    chnage image

--- src/page/offerings/index.js
+++ src/page/offerings/index.js
@@ -5,62 +5,74 @@ const offerings = [
     {
         title: "Data Analytics Dashboards",
         description: "Visualize and analyze healthcare data for better decision-making and improved patient outcomes.",
-        image: "/images/offering/dashboard-image.png"
+        image: "/images/offering/dashboard-image.png",
+        grid: 7
     },
     {
         title: "Appointment Management",
         description: "Streamline the booking process and manage appointments efficiently to reduce no-shows and improve patient satisfaction.",
-        image: "/images/offering/appointment-image.png"
+        image: "/images/offering/appointment-image.png",
+        grid: 5
     },
     {
         title: "Patient Information",
         description: "Securely manage and access patient records, ensuring data accuracy and confidentiality.",
-        image: "/images/offering/patient-image.png"
+        image: "/images/offering/patient-image.png",
+        grid: 8
     },
     {
         title: "E-Prescription Management",
         description: "Simplify prescription management with electronic prescriptions, reducing errors and improving efficiency.",
-        image: "/images/offering/prescription-image.png"
+        image: "/images/offering/prescription-image.png",
+        grid: 4
     },
     {
         title: "Clinical Workflow",
         description: "Optimize clinical workflows to enhance the productivity of healthcare providers and improve patient care.",
-        image: "/images/offering/workflow-image.png"
+        image: "/images/offering/workflow-image.png",
+        grid: 4
     },
     {
         title: "Telemedicine",
         description: "Expand healthcare reach with secure and reliable telemedicine services, providing care remotely.",
-        image: "/images/offering/telemedicine-image.png"
+        image: "/images/offering/telemedicine-image.png",
+        grid: 4
     },
     {
         title: "AI-driven Admin Automation",
         description: "Leverage AI to automate administrative tasks, reducing workload and improving operational efficiency.",
-        image: "/images/offering/automation-image.png"
+        image: "/images/offering/automation-image.png",
+        grid: 4
     },
     {
         title: "API Integration",
         description: "Integrate seamlessly with existing systems using robust APIs, enabling data exchange and interoperability.",
-        image: "/images/offering/api-image.png"
-    },
-    {
-        title: "Payments & Billing",
-        description: "Simplify billing and payments with automated systems, ensuring timely and accurate transactions.",
-        image: "/images/offering/billing-image.png"
+        image: "/images/offering/api-image.png",
+        grid: 4
     },
     {
         title: "Inventory Management",
         description: "Manage medical inventory efficiently, ensuring the availability of necessary supplies and reducing wastage.",
-        image: "/images/offering/inventory-image.png"
+        image: "/images/offering/inventory-image.png",
+        grid: 8
+    },
+    {
+        title: "Payments & Billing",
+        description: "Simplify billing and payments with automated systems, ensuring timely and accurate transactions.",
+        image: "/images/offering/billing-image.png",
+        grid: 4
     },
     {
         title: "Patient Portal",
         description: "Provide patients with a secure portal to access their health information, schedule appointments, and communicate with healthcare providers.",
-        image: "/images/offering/portal-image.png"
+        image: "/images/offering/portal-image.png",
+        grid: 5
     },
     {
         title: "Communications",
         description: "Enhance communication between healthcare providers and patients, ensuring timely and effective information exchange.",
-        image: "/images/offering/communication-image.png"
+        image: "/images/offering/communication-image.png",
+        grid: 3
     }
 ];
 
@@ -70,7 +82,7 @@ export default function Offerings() {
         <>
             <div className="offering_home">
                 <Container>
-                    <Row className='g-4'>
+                    <Row className='g-0'>
                         <Col xs={12}>
                             <div className='text-center text-md-start'>
                                 <h2
@@ -86,21 +98,27 @@ export default function Offerings() {
                                 </p>
                             </div>
                         </Col>
-                        {offerings.map((offering, index) =>
-                            <Col key={index} xs={12} md={4}>
-                                <Card className="offering-card h-100">
+                        <Col xs={12}>
+                            <Row className='g-0'>
+                                {offerings.map((offering, index) =>
+                                    <Col key={index} xs={12} md={offering.grid}>
+                                        {/* <Card className="offering-card h-100">
                                     <Card.Img variant="top" src={offering.image} />
                                     <Card.Body className="d-flex flex-column">
                                         <Card.Title>{offering.title}</Card.Title>
                                         <Card.Text className="flex-grow-1">{offering.description}</Card.Text>
                                     </Card.Body>
-                                </Card>
-                                {/* <div className='p-3 border h-100 d-flex flex-column justify-content-between'>
-                                    <h6>{offering.title}</h6>
-                                    <img src={offering.image} className='w-100' alt='images' />
-                                </div> */}
-                            </Col>
-                        )}
+                                </Card> */}
+                                        <div className=' offering-card p-3 mt-5 border h-100 d-flex flex-column justify-content-between'>
+                                            <p className='body-2'>{offering.title}</p>
+                                            <div className='flex-grow-1 d-flex justify-content-center align-items-center'>
+                                                <img src={offering.image} className='w-100' alt='images' />
+                                            </div>
+                                        </div>
+                                    </Col>
+                                )}
+                            </Row>
+                        </Col>
                     </Row>
                 </Container>
             </div>
